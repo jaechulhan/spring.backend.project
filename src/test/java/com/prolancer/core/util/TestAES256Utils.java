61be371c8ddf887e8642a7d4068262eed9f736d5
jaechulhan <jaechulhan@gmail.com>
2022-01-25 08:16:48 -0500

    Name: jaechulhan Description: Update TestAES256Utils

--- src/test/java/com/prolancer/core/util/TestAES256Utils.java
+++ src/test/java/com/prolancer/core/util/TestAES256Utils.java
@@ -4,20 +4,12 @@ import com.prolancer.core.common.utils.AES256Utils;
 
 public class TestAES256Utils {
     public static void main(String[] args) {
-        String arrStr[] = {"jaechulhan@gmail.com", "+1 (301) 265-6220", "password123!@#"};
+        String arrStr[] = {"test@test.com", "+1 (123) 456-7890", "password123!@#"};
 
         for (String orgStr : arrStr) {
             String encStr = AES256Utils.encrypt(orgStr);
             String decStr = AES256Utils.decrypt(encStr);
             System.out.println("Origin : " + orgStr);
-            System.out.println("Encrypted : " + encStr);
-            System.out.println("Decrypted : " + decStr);
-        }
-
-        String arrEncStr[] = {"_AG-VM6Rgv6cvlgUwJs_UDtC1AHKQFIn90RxgWXzbXk", "Z129JOHhjHPpDlZOPtEeUXnEUKK63PboqL6_JCBfE_k", "5vswIXQm3lr8U6rVdo93dQ"};
-
-        for (String encStr : arrEncStr) {
-            String decStr = AES256Utils.decrypt(encStr);
             System.out.println("Encrypted : " + encStr);
             System.out.println("Decrypted : " + decStr);
         }
